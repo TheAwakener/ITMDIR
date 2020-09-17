6977b45f33120a190ffa195e6764cbdaf9776f6a
Julian Ramirez Gomez <julianbit.ramirez@gmail.com>
2020-09-17 00:20:00 -0500

    Update containter_deploy.sh

--- LaboratorioMod1/containter_deploy.sh
+++ LaboratorioMod1/containter_deploy.sh
@@ -4,48 +4,39 @@ swname="switch01"
 
 
 function pkg_install(){
-        vsw_path="https://www.openvswitch.org/releases/openvswitch-2.13.1.tar.gz"
-
-        printf "[U] Instalando llaves del repositorio de docker..."
+        
+        printf "[INSTALL]: Instalando llaves del repositorio de docker...\n"
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null
-	printf "[OK]\n"
 
-        printf "[U] Agregando el repositorio de docker..."
+        printf "[INSTALL]: Agregando el repositorio de docker...\n"
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null
-	printf "[OK]\n"
 
-        printf "[U] Actualizando repositiorios..."
+        printf "[UPDATE]: Actualizando repositiorios...\n"
         sudo apt-get update -y > /dev/null
-	printf "[OK]\n"
 
-        printf "[I] Instalando docker..."
+        printf "[INSTALL]: Instalando docker...\n"
         sudo apt-get install -y docker-ce > /dev/null
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null
 	sudo chmod +x /usr/local/bin/docker-compose
-	printf "[OK]\n"
 
-        printf "[I] Instalando compilador GCC..."
+        printf "[INSTALL]: Instalando compilador GCC...\n"
         sudo apt-get install -y gcc > /dev/null
-	printf "[OK]\n"
 
-        printf "[I] Instalando Open vSwitch..."
+        printf "[INSTALL]: Instalando Open vSwitch...\n"
         sudo apt-get install -y openvswitch-switch > /dev/null && sudo apt-get install -y openvswitch-common > /dev/null
-	printf "[OK]\n\n"
 }
 
 
 function config_switch(){
-	printf "[C] Configurando switch virtual [Nombre: switch01]... "
+	printf "[CONFIG]: Configurando switch virtual [Nombre: switch01]...\n"
 	sudo ovs-vsctl add-br $swname
 	sudo ovs-vsctl set bridge $swname other_config:mac-aging-time=60
 	sudo ovs-vsctl set bridge $swname other_config:mac-table-size=2048
-	printf "[OK] Switch listo!\n"
-
 }
 
 
 function network_deploy(){
-	printf "[D] Desplegango red virtual..."
+	printf "[DEPLOY]: Desplegango red virtual...\n"
 	cont_names=("kali" "workstation" "server" "gateway" "nmonitor")
 	for container in ${cont_names[*]}; do
 		contid=$(sudo docker ps -q -f name=$container)
@@ -65,46 +56,39 @@ function network_deploy(){
 			sudo ovs-docker add-port $swname eth0 $contid
 		fi
 	done
-	printf "[OK]\n"
 
 	nmiface=$(sudo ovs-dpctl show | grep -P "port 6:.*" | cut -d ":" -f 2 | tr -d " ")
 
-	printf "[C] configurando port-mirror en interface $nmiface... "
+	printf "\t[CONFIG] configurando port-mirror en interface $nmiface...\n"
         sudo ovs-vsctl --id=@p get port $nmiface -- --id=@m create mirror name=m0 select-all=true output-port=@p -- set bridge $swname mirrors=@m
-	printf "[OK]\n"
 
-	printf "[T] Ajustando interface modo promiscuo en Nmonitor... "
+	printf "\t[CONFIG]: Ajustando interface modo promiscuo en Nmonitor...\n"
         sudo docker exec nmonitor ip link set eth0 promisc on
-        printf "[OK]\n"
 
-        printf "[T] Desplegando Zeek... "
+        printf "\t[CONFIG]: Desplegando Zeek...\n"
         sudo docker exec nmonitor zeekctl deploy > /dev/null
-        printf "[OK]\n"
 
-        printf "[T] Iniciando servicios en VLAN de servidores... "
+        printf "\t[CONFIG]: Iniciando servicios en VLAN de servidores...\n"
         sudo docker exec -d server /bin/services.sh > /dev/null
 	sleep 10
 	sudo docker exec -d server service apache2 start > /dev/null
-        printf "[OK]\n"
 
-	printf "[T] Ajustando IPTABLES..."
+	printf "\t[CONFIG]: Ajustando IPTABLES...\n"
 	sudo docker exec -d server iptables -A INPUT -s 10.1.0.0/24 -d 0.0.0.0/0 -j ACCEPT > /dev/null
 	sudo docker exec -d server iptables -P INPUT DROP > /dev/null
 	printf "[OK]\n"
 	
-	printf "[T] Configurando router/firewall... "
+	printf "[CONFIG] Configurando router/firewall...\n"
 	sudo docker exec -d gateway su vyos sg vyattacfg -c scripts/provision_script.sh
-	printf "[OK]\n"
 }
 
 function container_deploy(){
 	stcont=""
-        printf "[D] Descargando script YAML para despliegue de contenedores..."
+        printf "[DEPLOY]: Descargando script YAML para despliegue de contenedores...\n"
 	$(wget -q --no-check-certificate https://raw.githubusercontent.com/jramirezgo/ITMDIR/master/LaboratorioMod1/docker-compose.yaml)
-	printf "[OK]\n"
-	printf "[I] Desplegando contenedores... "
+	
+	printf "[DEPLOY]: Desplegando contenedores...\n"
 	$(sudo docker-compose up -d)
-	printf "[OK] Listo!\n"
 }
 
 
